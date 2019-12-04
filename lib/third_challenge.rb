12c60cbd862430412b0bed5c65070328af7fa2ab
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-04 18:09:50 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Montague", age: "50"
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
         name: "Lady Capulet", age: "51"
